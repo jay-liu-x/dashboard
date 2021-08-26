1e4f8341db7cf56724e9669ce4744667754034aa
jay-liu-x <jayliu925@gmail.com>
2021-08-26 17:09:09 -0400

    almost done

--- src/app/market-movers/market-movers.component.ts
+++ src/app/market-movers/market-movers.component.ts
@@ -1,86 +1,96 @@
 import { Component, OnInit } from '@angular/core';
-import{MarketMoversService} from  'src/services/market-movers.service';
+import { MarketMoversService } from 'src/services/market-movers.service';
 
 @Component({
   selector: 'app-market-movers',
   templateUrl: './market-movers.component.html',
-  styleUrls: ['./market-movers.component.css']
+  styleUrls: ['./market-movers.component.css'],
 })
 export class MarketMoversComponent implements OnInit {
-  DOWJONESData:any ={percentage:'-0.8%' , difference:'-1.0',show: '0.0%'}
-  SP500Data: any = {percentage:'5.0%' , difference:'2.0',show: '0.0%'}
-  NASDQData: any = {percentage:'1.2%' , difference:'3.0',show: '0.0%'}
-  BondData: any = {percentage:'4.0%' , difference:'4.0',show: '0.0%'}
+  DOWJONESData: any = {
+    percentage: '-0.8%',
+    difference: '-1.0',
+    show: '-1.5%',
+  };
+  SP500Data: any = { percentage: '5.0%', difference: '2.0', show: '1.1%' };
+  NASDQData: any = { percentage: '1.2%', difference: '3.0', show: '2.0%' };
+  BondData: any = { percentage: '4.0%', difference: '4.0', show: '5.3%' };
 
-  constructor(private marketmoversService:MarketMoversService) { }
+  constructor(private marketmoversService: MarketMoversService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
 
-   /* this.marketmoversService.getDOWJONES()
-      .subscribe((data:any)=>{
-        console.log(data)
-        this.DOWJONESData['percentage'] =  (data['prices'][0]['close']/ data['prices'][1]['close'])*100-100
-        this.DOWJONESData['percentage'] = this.DOWJONESData['percentage'].toFixed(2) +'%'
-        this.DOWJONESData['difference'] = (data['prices'][0]['close'] - data['prices'][1]['close']).toFixed(2)
-        //initialization
-        this.DOWJONESData['show'] = this.DOWJONESData['percentage']
-        
-      })
+  showPercentage() {
+    this.DOWJONESData['show'] = this.DOWJONESData['percentage'];
+    this.SP500Data['show'] = this.SP500Data['percentage'];
+    this.NASDQData['show'] = this.NASDQData['percentage'];
+    this.BondData['show'] = this.BondData['percentage'];
+  }
+  showDifference() {
+    this.DOWJONESData['show'] = this.DOWJONESData['difference'];
+    this.SP500Data['show'] = this.SP500Data['difference'];
+    this.NASDQData['show'] = this.NASDQData['difference'];
+    this.BondData['show'] = this.BondData['difference'];
+  }
+  switchMode() {}
 
-      this.marketmoversService.getSP500()
-      .subscribe((data:any)=>{
-        console.log(data)
-        this.SP500Data['percentage'] = (data['prices'][0]['close']/ data['prices'][1]['close'])*100-100
-        this.SP500Data['percentage'] = this.SP500Data['percentage'].toFixed(2) +'%'
-        this.SP500Data['difference'] = (data['prices'][0]['close'] - data['prices'][1]['close']).toFixed(2)
-        //initialization
-        this.SP500Data['show'] = this.SP500Data['percentage']
-        
-      })
-
-      this.marketmoversService.get10YearBond()
-      .subscribe((data:any)=>{
-        console.log(data)
-        this.BondData['percentage'] = (data['prices'][0]['close']/ data['prices'][1]['close'])*100-100
-        this.BondData['percentage'] = this.BondData['percentage'].toFixed(2) +'%'
-        this.BondData['difference'] = (data['prices'][0]['close'] - data['prices'][1]['close']).toFixed(2)
-        //initialization
-        this.BondData['show'] = this.BondData['percentage']
-        
-      })
-
-      this.marketmoversService.getNASDQ()
-      .subscribe((data:any)=>{
-        console.log(data)
-        this.NASDQData['percentage'] = (data['prices'][0]['close']/ data['prices'][1]['close'])*100-100
-        this.NASDQData['percentage'] = this.NASDQData['percentage'].toFixed(2) +'%'
-        this.NASDQData['difference'] = (data['prices'][0]['close'] - data['prices'][1]['close']).toFixed(2)
-        //initialization
-        this.NASDQData['show'] = this.NASDQData['percentage']
-        
-      })*/
-
-
-
-
-
+  refreshDJ() {
+    this.marketmoversService.getDOWJONES().subscribe((data: any) => {
+      console.log(data);
+      this.DOWJONESData['percentage'] =
+        (data['prices'][0]['close'] / data['prices'][1]['close']) * 100 - 100;
+      this.DOWJONESData['percentage'] =
+        this.DOWJONESData['percentage'].toFixed(2) + '%';
+      this.DOWJONESData['difference'] = (
+        data['prices'][0]['close'] - data['prices'][1]['close']
+      ).toFixed(2);
+      //initialization
+      this.DOWJONESData['show'] = this.DOWJONESData['percentage'];
+    });
   }
 
-  showPercentage(){
-    this.DOWJONESData['show'] = this.DOWJONESData['percentage']
-    this.SP500Data['show'] = this.SP500Data['percentage']
-    this.NASDQData['show'] = this.NASDQData['percentage']
-    this.BondData['show'] = this.BondData['percentage']
-  }
-  showDifference(){
-
-    this.DOWJONESData['show'] = this.DOWJONESData['difference']
-    this.SP500Data['show'] = this.SP500Data['difference']
-    this.NASDQData['show'] = this.NASDQData['difference']
-    this.BondData['show'] = this.BondData['difference']
-  }
-  switchMode(){
-    
+  refreshSP() {
+    this.marketmoversService.getSP500().subscribe((data: any) => {
+      console.log(data);
+      this.SP500Data['percentage'] =
+        (data['prices'][0]['close'] / data['prices'][1]['close']) * 100 - 100;
+      this.SP500Data['percentage'] =
+        this.SP500Data['percentage'].toFixed(2) + '%';
+      this.SP500Data['difference'] = (
+        data['prices'][0]['close'] - data['prices'][1]['close']
+      ).toFixed(2);
+      //initialization
+      this.SP500Data['show'] = this.SP500Data['percentage'];
+    });
   }
 
+  refreshBond() {
+    this.marketmoversService.get10YearBond().subscribe((data: any) => {
+      console.log(data);
+      this.BondData['percentage'] =
+        (data['prices'][0]['close'] / data['prices'][1]['close']) * 100 - 100;
+      this.BondData['percentage'] =
+        this.BondData['percentage'].toFixed(2) + '%';
+      this.BondData['difference'] = (
+        data['prices'][0]['close'] - data['prices'][1]['close']
+      ).toFixed(2);
+      //initialization
+      this.BondData['show'] = this.BondData['percentage'];
+    });
+  }
+
+  refreshNasdaq() {
+    this.marketmoversService.getNASDQ().subscribe((data: any) => {
+      console.log(data);
+      this.NASDQData['percentage'] =
+        (data['prices'][0]['close'] / data['prices'][1]['close']) * 100 - 100;
+      this.NASDQData['percentage'] =
+        this.NASDQData['percentage'].toFixed(2) + '%';
+      this.NASDQData['difference'] = (
+        data['prices'][0]['close'] - data['prices'][1]['close']
+      ).toFixed(2);
+      //initialization
+      this.NASDQData['show'] = this.NASDQData['percentage'];
+    });
+  }
 }
--- src/services/market-movers.service.ts
+++ src/services/market-movers.service.ts
@@ -2,63 +2,62 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MarketMoversService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-  getDOWJONES(){
-   
-    return this.http.get(`https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v3/get-historical-data?symbol=%5EDJI&region=US`,
-    {
-      headers:{
-        "x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
-		    "x-rapidapi-key": "cb81b4cfe9msh264914b7df45c4cp1d3300jsnb63e0f84a578",
-
-      },
-    }
+  getDOWJONES() {
+    return this.http.get(
+      `https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v3/get-historical-data?symbol=%5EDJI&region=US`,
+      {
+        headers: {
+          'x-rapidapi-host': 'apidojo-yahoo-finance-v1.p.rapidapi.com',
+          'x-rapidapi-key':
+            'b0062ec590mshff694b3ce63c71fp12cfccjsnf0e6494799a0',
+        },
+      }
     );
   }
 
-  getSP500(){
-    return this.http.get(`https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v3/get-historical-data?symbol=%5EGSPC&region=US`,
-    {
-      headers:{
-        "x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
-		    "x-rapidapi-key": "cb81b4cfe9msh264914b7df45c4cp1d3300jsnb63e0f84a578"
-
-      },
-    }
+  getSP500() {
+    return this.http.get(
+      `https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v3/get-historical-data?symbol=%5EGSPC&region=US`,
+      {
+        headers: {
+          'x-rapidapi-host': 'apidojo-yahoo-finance-v1.p.rapidapi.com',
+          'x-rapidapi-key':
+            'b0062ec590mshff694b3ce63c71fp12cfccjsnf0e6494799a0',
+        },
+      }
     );
   }
 
-  get10YearBond(){
-    return this.http.get(`https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v3/get-historical-data?symbol=%5ETNX&region=US`,
-    {
-      headers:{
-        "x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
-		    "x-rapidapi-key": "cb81b4cfe9msh264914b7df45c4cp1d3300jsnb63e0f84a578"
-
-      },
-    }
+  get10YearBond() {
+    return this.http.get(
+      `https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v3/get-historical-data?symbol=%5ETNX&region=US`,
+      {
+        headers: {
+          'x-rapidapi-host': 'apidojo-yahoo-finance-v1.p.rapidapi.com',
+          'x-rapidapi-key':
+            'b0062ec590mshff694b3ce63c71fp12cfccjsnf0e6494799a0',
+        },
+      }
     );
   }
 
-  getNASDQ(){
-    return this.http.get(`https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v3/get-historical-data?symbol=%5EIXIC&region=US`,
-    {
-      headers:{
-        "x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
-		    "x-rapidapi-key": "cb81b4cfe9msh264914b7df45c4cp1d3300jsnb63e0f84a578"
-
-      },
-    }
+  getNASDQ() {
+    return this.http.get(
+      `https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v3/get-historical-data?symbol=%5EIXIC&region=US`,
+      {
+        headers: {
+          'x-rapidapi-host': 'apidojo-yahoo-finance-v1.p.rapidapi.com',
+          'x-rapidapi-key':
+            'b0062ec590mshff694b3ce63c71fp12cfccjsnf0e6494799a0',
+        },
+      }
     );
   }
-
-
 
   //"https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v3/get-historical-data?symbol=%5EGSPC&region=US"
 }
-
